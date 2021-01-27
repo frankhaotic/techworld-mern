70879c2420d83014b08a13b16c39549e294d7968
Frankie Flood <fsfloody@gmail.com>
2021-01-27 17:54:19 +0000

    add colors package to format console messages

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 // npm package imports
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 
 // custom files for import
 import connectDB from "./config/db.js";
@@ -41,5 +42,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
