c5ec2c0083a18eb3ef4af6a135a06b9ab273d97a
Frankie Flood <fsfloody@gmail.com>
2021-02-11 20:58:40 +0000

    create the getOrderById controller & route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 const router = express.Router();
 
 // Import Controller Methods
-import { addOrderItems } from "../controllers/orderControllers.js";
+import { addOrderItems, getOrderById } from "../controllers/orderControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
