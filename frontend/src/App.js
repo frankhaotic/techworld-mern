d92c1571f937edddf6684e69faa18d2033580747
Frankie Flood <fsfloody@gmail.com>
2021-02-14 16:18:00 +0000

    create redux for showing user list to admin & build the UserListScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
         </Container>
       </main>
       <Footer />
