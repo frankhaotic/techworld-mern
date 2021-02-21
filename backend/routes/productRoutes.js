405ed766851eb3bd21ccff73ec3ad467791e9595
Frankie Flood <fsfloody@gmail.com>
2021-02-21 19:42:13 +0000

    create backend route to get top 3 products & redux in frontend to get those products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,15 +9,17 @@ import {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productControllers.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
   .delete(protect, isAdmin, deleteProduct)
   .put(protect, isAdmin, updateProduct);
-router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
